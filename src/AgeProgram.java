66e49531d0737b36d37a1ecc2f91faa275ed6fe2
kulakshi <your.email@example.com>
2023-10-09 15:38:22 +0300

    finalized the program

--- src/AgeProgram.java
+++ src/AgeProgram.java
@@ -2,7 +2,7 @@
 public class AgeProgram {
 
 	public static void main(String[] args) {
-		int age = 10; 
+		int age = 40; 
 		if (age > 0 && age < 18) { 
 		System.out.println("You are underage.");
 		 } else { 
@@ -14,8 +14,18 @@ public class AgeProgram {
 			 System.out.println("You can drive a car");
 		 }
 		 
-		 
+		if (age == 10 || age == 20 || age == 30 ||age == 40|| age == 50 || age == 60|| age == 70 || age == 80 
+				||age == 90||age == 100||age == 110 ||age == 120) {
+			System.out.println("Anniversary Party!!");
+		}
 		
-	}
+		if (age == 100) {
+			 System.out.println("congratulations!");
+			 System.out.println("congratulations!");
+			 System.out.println("congratulations!");
+		 }
+		
+		if (age>=40&&age<50)
+			System.out.println("wish happy mid-life.");	}
 	
-}
+}		
