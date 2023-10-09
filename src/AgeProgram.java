f4733589ef078f266678890e493ba6ca3ea8cf18
kulakshi <your.email@example.com>
2023-10-09 15:16:47 +0300

    finalized first case of age program

--- src/AgeProgram.java
+++ src/AgeProgram.java
@@ -2,8 +2,20 @@
 public class AgeProgram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int age = 10; 
+		if (age > 0 && age < 18) { 
+		System.out.println("You are underage.");
+		 } else { 
+		 System.out.println("You are an adult."); 
+		 } 
+		 System.out.println("Press space to exit.");	
+		 
+		 if (age == 18) {
+			 System.out.println("You can drive a car");
+		 }
+		 
+		 
+		
 	}
-
+	
 }
